6c10abc85d50b4a2e002256fa2a5a5af83d1cc0a
Prince Mpho Msimango <princemsimango27@gmail.com>
2025-11-06 03:28:48 +0200

    made changes

--- src/Homepage/Article/PlantDIY.jsx
+++ src/Homepage/Article/PlantDIY.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from "./plantDIY.module.css";
+import styles from "./PlantDIY.module.css";
+
 import { Link } from "react-router-dom";
  
 function PlantDIY() {
